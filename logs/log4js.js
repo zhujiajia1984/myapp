e3bfbbfbb3b2733a9829af3217ea9957ada8994c
zhujiajia <772978409@qq.com>
2017-12-08 19:13:56 +0800

    去除mongodb

---
+++ logs/log4js.js
@@ -0,0 +1,41 @@
+/**
+ * Created by zjj on 2017/3/30.
+ */
+// 引入日志模块
+var log4js = require("log4js");
+log4js.configure({
+	appenders: {
+		out: {
+			type: 'stdout',
+			layout: { type: 'colored' }
+		},
+		console: {
+			type: 'console',
+		},
+		log_date: {
+			type: 'dateFile',
+			filename: '/home/myapp/logs/log_date/dateLog',
+			alwaysIncludePattern: true,
+			pattern: "-yyyy-MM-dd.log",
+			keepFileExt: true,
+		},
+		log_location: {
+			type: 'file',
+			filename: '/home/myapp/logs/log_location/location.log',
+			maxLogSize: 4194304, //单位Byte,4MB分割
+			keepFileExt: true,
+		}
+	},
+	categories: {
+		default: { appenders: ['log_date', 'out'], level: 'ALL' }, //默认
+		location: { appenders: ['log_location', 'out'], level: 'info' }
+	}
+})
+
+// 生成使用对象
+var dateLog = log4js.getLogger('default');
+var locationLog = log4js.getLogger('location');
+
+// 输出对象
+exports.logger = dateLog;
+exports.loggerLocation = locationLog;
---
+++ redis.js
@@ -0,0 +1,18 @@
+﻿var redis = require('redis');
+
+// RedisClient：连接Redis服务器
+var portR = '6379';
+var ipR = '127.0.0.1';
+var optionR = {auth_pass: 'zjj15202185069'};
+var redisClient = redis.createClient(portR, ipR, optionR);
+
+// redis服务器连接成功事件
+redisClient.on('ready', function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connect redis server OK');
+    }
+})
+
+module.exports = redisClient;
