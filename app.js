79f404f38b4763bdf9be37063a6104eaee9d6093
zhujiajia <772978409@qq.com>
2017-12-08 22:35:59 +0800

    更新日志打印功能

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var log4js = require('log4js');
+var logger = require('./logs/log4js').logger;
 
 
 // 自定义页面
@@ -31,6 +32,9 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// http日志服务
+app.use(log4js.connectLogger(logger, { level: 'auto' }));
 
 //
 app.use(bodyParser.text({ type: 'text/xml' })); // 将请求体中的xml解析为字符串
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var logger = require('../logs/log4js').logger;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	logger.info("home page");
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
